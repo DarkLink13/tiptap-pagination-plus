311bdec32aad827ad5016e4e4abe7de2f32f7a8c
Jorge Amado <darklink1302@gmail.com>
2025-10-19 21:30:05 -0400

    Update gitignore

---
+++ dist/utils.d.ts
@@ -0,0 +1,4 @@
+import { PaginationPlusOptions } from "./PaginationPlus";
+import { PageSize } from "./constants";
+export declare const updateCssVariables: (targetNode: HTMLElement, config: PaginationPlusOptions) => void;
+export declare const getPageSize: (height: number, width: number, marginTop: number, marginBottom: number, marginLeft: number, marginRight: number) => PageSize;
